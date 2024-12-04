5f1e0449e7d1fc745b616e095f32f6217b389578
Homin <coc1381@naver.com>
2024-12-04 16:41:26 +0900

    feat: get book data from api server - use server action to get data - use server component to fetch data faster

---
+++ src/actions/get-books/index.ts
@@ -0,0 +1,45 @@
+'use server'
+
+import { GetBooksResponse } from '@/schemas/book'
+import { ActionResult } from '../action-result'
+
+export const getBooks = async (): Promise<ActionResult<GetBooksResponse>> => {
+  try {
+    const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
+    if (!baseUrl) {
+      throw new Error('API base URL is not defined.')
+    }
+
+    const response = await fetch(`${baseUrl}/api/books`)
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch books')
+    }
+
+    const { success, data } = await response.json()
+
+    if (success) {
+      return {
+        status: 'success',
+        data: data,
+      }
+    } else {
+      return {
+        status: 'error',
+        error: '데이터의 형식이 올바르지 않습니다.',
+      }
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        status: 'error',
+        error: error.message,
+      }
+    }
+
+    return {
+      status: 'error',
+      error: '알 수 없는 에러가 발생했습니다. 다시 시도해주세요',
+    }
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
+import { getBooks } from '@/actions/get-books'
+
+export default async function Home() {
+  const result = await getBooks()
+
+  if (result.status === 'success') {
+    const { books } = result.data
+    console.log(books)
+  } else {
+    throw new Error(result.error)
+  }
+
   return <div>Home</div>
 }
