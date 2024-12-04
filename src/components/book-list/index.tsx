832234c33501de3f931cef2c22a3e7a83eb83315
Homin <coc1381@naver.com>
2024-12-05 00:29:09 +0900

    feat: set filtering - implemented filtering by titles and authors

--- src/components/book-list/index.tsx
+++ src/components/book-list/index.tsx
@@ -1,24 +1,7 @@
 import { GetBooksResponse } from '@/schemas/book'
+import { DataTable } from '../data-table'
+import { columns } from '../columns'
 
-export default function BookList({ books, pagination }: GetBooksResponse) {
-  return (
-    <div>
-      <h2>Books</h2>
-      {books.map((book) => (
-        <div key={book.id}>
-          <h3>{book.title}</h3>
-          <p>{book.author}</p>
-          <p>{book.subject}</p>
-          <p>Quantity: {book.quantity}</p>
-          <p>Sales Volume: {book.salesVolume}</p>
-        </div>
-      ))}
-      <div>
-        <p>
-          Page {pagination.currentPage} of {pagination.totalPages}
-        </p>
-        <p>Total Books: {pagination.totalBooks}</p>
-      </div>
-    </div>
-  )
+export default function BookList({ books }: GetBooksResponse) {
+  return <DataTable columns={columns} data={books} />
 }
---
+++ src/components/columns/index.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { ColumnDef } from '@tanstack/react-table'
+import { MoreHorizontal } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+import { BookListItem } from '@/schemas/book'
+
+export const columns: ColumnDef<BookListItem>[] = [
+  {
+    accessorKey: 'title',
+    header: 'Title',
+  },
+  {
+    accessorKey: 'author',
+    header: 'Author',
+  },
+  {
+    accessorKey: 'quantity',
+    header: 'Quantity',
+  },
+  {
+    id: 'actions',
+    cell: () => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem>edit quantity</DropdownMenuItem>
+            <DropdownMenuItem>delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  },
+]
